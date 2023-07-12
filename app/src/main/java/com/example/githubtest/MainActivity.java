5e8965e4f7a29367ffa13d2de601ee4f95855c42
Aaseka <dudebrampton@gmail.com>
2023-07-12 15:47:51 -0400

    just saying hello

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TODO: ASAHISH IS A BITCH
+
+        System.out.println("Hello Ruween");
     }
 }
