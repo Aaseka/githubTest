6efde6ba26b8d60a6a8aa037de7eb0caa4517a2e
Ruweener <139298990+Ruweener@users.noreply.github.com>
2023-07-12 23:09:36 -0400

    balls

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.githubtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Hello Ruween");
+
+    }
+
+    public void onResister (View view) {
+        TextView firstName = findViewById(R.id.textView);
+        TextView lastName = findViewById(R.id.textView2);
+        TextView email = findViewById(R.id.textView3);
+
+        EditText editTxtFirstName = findViewById(R.id.enterFirstName);
+        EditText editTxtLastName = findViewById(R.id.enterLastName);
+        EditText editTxtEmail = findViewById(R.id.enterEmail);
+
+        firstName.setText("First Name: " + editTxtFirstName.getText().toString());
+        lastName.setText("Last Name: " + editTxtLastName.getText().toString());
+        email.setText("Email: " + editTxtEmail.getText().toString());
     }
 }
